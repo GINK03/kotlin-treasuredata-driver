8c1e50264fb87f6d63c78940364f4e28018185d7
nardtree <gim.kobayashi@gmail.com>
2017-04-12 17:21:59 +0900

    add downloader

---
+++ download-setup-jars.sh
@@ -0,0 +1,7 @@
+wget https://www.dropbox.com/s/mxivoqoj7qkaamf/jackson-dataformat-msgpack-0.8.13-SNAPSHOT.jar
+wget https://www.dropbox.com/s/lf5w63j3mclswp4/msgpack-core-0.8.13-SNAPSHOT.jar
+wget https://www.dropbox.com/s/z5sg3d30hdp2fgv/msgpack-java-0.8.13-SNAPSHOT.jar
+wget https://www.dropbox.com/s/f4h8zne2wsjw6r1/td-client-0.7.9-jar-with-dependencies.jar
+wget https://www.dropbox.com/s/e9wcqy9hxrrhs8c/TDHandler.jar
+wget https://www.dropbox.com/s/jm9k0yfu00aapgf/TreasureData.jar
+mv *.jar jars
