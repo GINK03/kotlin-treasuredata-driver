3700e165cb1643d5307287755e897ac4ed548a9e
nardtree <gim.kobayashi@gmail.com>
2017-04-21 12:46:34 +0900

    a

--- download-setup-jars.sh
+++ download-setup-jars.sh
@@ -4,4 +4,7 @@ wget https://www.dropbox.com/s/z5sg3d30hdp2fgv/msgpack-java-0.8.13-SNAPSHOT.jar
 wget https://www.dropbox.com/s/f4h8zne2wsjw6r1/td-client-0.7.9-jar-with-dependencies.jar
 wget https://www.dropbox.com/s/e9wcqy9hxrrhs8c/TDHandler.jar
 wget https://www.dropbox.com/s/jm9k0yfu00aapgf/TreasureData.jar
+wget https://www.dropbox.com/s/j4qnlf1dxahi5a1/gson-2.8.0.jar
+wget https://www.dropbox.com/s/l55z8wbs0umlofc/td-client-0.7.34-jar-with-dependencies.jar
+wget https://www.dropbox.com/s/l55z8wbs0umlofc/td-client-0.7.34-jar-with-dependencies.jar
 mv *.jar jars
